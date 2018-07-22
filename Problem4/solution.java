905b87f290eb6c85e6030a5c473813d903e04eea
baquer <baquerali@icloud.com>
2018-07-23 00:29:34 +0530

    Problem 4

---
+++ Problem4/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+				Scanner in = new Scanner(System.in);
+ while(true)
+ {
+  int a = in.nextInt();
+  if(a != 42){
+    System.out.println(a);
+  } else
+  {
+   break;
+  }
+ }
+	}
+}
